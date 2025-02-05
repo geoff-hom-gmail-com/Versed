a2cfe6a63332ed5c8226ed98b9dc1b8a31e6db10
Geoff Hom <geoff.hom+git@gmail.com>
2025-02-05 02:19:01 -0600

    Refactor to make more browsable.
    - (AddView) Encapsulate buttons. Reorder. Add code marks.

--- Versed/Views/AddView.swift
+++ Versed/Views/AddView.swift
@@ -14,72 +14,81 @@ struct AddView: View {
                 TextFieldSection(for: .after, text: $afterText)
                 TextFieldSection(for: .reference, text: $referenceText)
             }
-            .scrollDismissesKeyboard(.immediately)
             .toolbar {
-                // TODO: - make more browsable?
                 ToolbarItem(placement: .cancellationAction) {
-                    Button(AppConstant.Label.reset) {
-                        reset()
-                    }
-                    .disabled(isNoText)
+                    resetButton
                 }
                 ToolbarItem(placement: .confirmationAction) {
-                    Button(AppConstant.Label.done) {
-                        var newIndex = 0
-                        if let maxIndex = userTextsOrderReversed.first?.index {
-                            newIndex = maxIndex + 1
-                        }
-                        
-                        let passage = Passage(index: newIndex, before: beforeText, goal: goalText, after: afterText, reference: referenceText)
-                        modelContext.insert(passage)
-                        
-                        // (Note) Not sure if needed on device. But, in Xcode preview, helps.
-                        do {
-                            try modelContext.save()
-                        } catch {
-                            print("Failed to save: \(error)")
-                        }
-                                                
-                        reset()
-                        // (toDo) after adding/saving, user gets feedback. (a badge on Texts ("!", "new" etc)  It depends, too. Usually, the text needs work like beats. But it's possible it's perfectly fine. In which case it's more like it's new and the user just has to okay it in Texts.
-                    }
-                    .disabled(isNoText)
+                    doneButton
                 }
             }
+            .scrollDismissesKeyboard(.immediately)
         }
     }
     
-    // MARK: - (parameters?)
-    // TODO: - tidy marks below
-
+    // MARK: - (state properties)
     @State private var beforeText: String = ""
     @State private var goalText: String = ""
     @State private var afterText: String = ""
     @State private var referenceText: String = ""
-    
-    // (Note) Why not [String]? Won't work in reset().
-    private var inputTexts: [Binding<String>] {
-        [$beforeText, $goalText, $afterText, $referenceText]
+
+    // MARK: - (buttons)
+
+    private var resetButton: some View {
+        Button(AppConstant.Label.reset) {
+            reset()
+        }
+        .disabled(isNoText)
     }
     
-    // (Goal) Buttons are disabled, if no text.
-    private var isNoText: Bool {
-        inputTexts.allSatisfy { $0.wrappedValue.isEmpty }
+    private var doneButton: some View {
+        Button(AppConstant.Label.done) {
+            // (Goal) The text has the right index.
+            var newIndex = 0
+            if let maxIndex = userTextsOrderReversed.first?.index {
+                newIndex = maxIndex + 1
+            }
+            
+            let passage = Passage(index: newIndex,
+                                  before: beforeText,
+                                  goal: goalText,
+                                  after: afterText,
+                                  reference: referenceText)
+            modelContext.insert(passage)
+            
+            // (Note) Not sure if needed on device. But, in Xcode preview, helps.
+            do {
+                try modelContext.save()
+            } catch {
+                print("Failed to save: \(error)")
+            }
+                                    
+            reset()
+        }
+        .disabled(isNoText)
     }
-    
-    @Environment(\.modelContext) private var modelContext
 
-    // (Goal) The app can calculate the index for new texts.
-    @Query(filter: #Predicate<Passage> { $0.isExample == false },
-           sort: \.index, order: .reverse)
-    var userTextsOrderReversed: [Passage]
-
-    
     // (Goal) The user sees the view reset.
     // (Note) Was worried about this being too abrupt. But seems okay.
     private func reset() {
         inputTexts.forEach { $0.wrappedValue = "" }
     }
+    
+    // (Goal) When are buttons disabled? If no text.
+    private var isNoText: Bool {
+        inputTexts.allSatisfy { $0.wrappedValue.isEmpty }
+    }
+    
+    // (Note) Why not [String]? Won't work in reset().
+    private var inputTexts: [Binding<String>] {
+        [$beforeText, $goalText, $afterText, $referenceText]
+    }
+
+    @Query(filter: #Predicate<Passage> { $0.isExample == false },
+           sort: \.index, order: .reverse)
+    var userTextsOrderReversed: [Passage]
+    
+    @Environment(\.modelContext) private var modelContext
 }
 
 // MARK: - (preview)
