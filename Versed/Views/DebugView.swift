f54fe39554a5379f53f2d5c1cb2e3dce8899e41a
Geoff Hom <geoff.hom+git@gmail.com>
2025-02-08 20:23:06 -0600

    (DebugView) The tester can add multiverse.
    - Add code marks.
    - Remove old debug stuff for Verse.
    - Add button to add multiverse.
    - Prep button to add universe.

--- Versed/Views/DebugView.swift
+++ Versed/Views/DebugView.swift
@@ -1,122 +1,184 @@
 import SwiftUI
 import AVFAudio
 
-// (Goal) The coder/tester can tweak stuff.
+// MARK: - (DebugView)
+// (Goal) The dev and tester can tweak stuff.
 struct DebugView: View {
-    @Environment(\.modelContext) private var modelContext
-    
+    // MARK: - (body)
     var body: some View {
+        Spacer()
+        deleteDataButton
+        Spacer()
+        addMultiverseButton
+        Spacer()
+        addUniverseButton
+        Spacer()
+        
         VStack {
-            // (Goal) The coder can check available voices.
-            // (note: 1.2.25) This was done to see how to use higher-quality voices (e.g., Siri voices) for TTS. So far, Siri voices are unavailable. The user can download enhanced/premium voices. Also, she can make Personal Voices.
-            Button("Print available voices") {
-                let voices = AVSpeechSynthesisVoice.speechVoices()
-                voices.forEach {
-                    print($0)
-                }
-            }
-            Spacer()
-            
-            // (Goal) The coder can delete all persistent data without going thru Terminal and deleting all preview data. Latter takes 30" to rebuild.
-            Button("Delete all persistent data") {
-                // deprecated; iOS 18+ use erase()
-                // Works. But may crash preview.
-                modelContext.container.deleteAllData()
-                
-                // (Note) Not sure if needed on device. But, in Xcode preview, helps? (maybe? can try erase() later) (and this is just for debugging)
-                do {
-                    try modelContext.save()
-                } catch {
-                    print("Failed to save: \(error)")
-                }
-            }
-            Spacer()
-            
-            // (Goal) The coder can add a user verse with a distant due date.
-            Button("Add user verse, with distant due date") {
-                let verse = Verse("A message from the future", dueDate: Date.distantFuture)
-                modelContext.insert(verse)
-            }
-            Spacer()
-
-            // (Goal) The coder can add a user verse with a past-due date.
-            Button("Add user verse, with past-due date") {
-                let verse = Verse("A message from the past", dueDate: Date.distantPast)
-                modelContext.insert(verse)
-            }
-            Spacer()
-            
-            // (Goal) The coder can add a shorter, realistic verse with a past-due date.
-            Button("Add realistic verse. Shorter. With past-due date") {
-                // (NIV) (John 15)
-                let text = """
-                    Apart from me you can do nothing. 
-                    """
-                let beats = """
-                    "Apart from me 
-                    
-                    you can do nothing."
-                    """
-                let cues = [Prompt(text: "When I forget about Jesus.", clarifier: "")]
-                let clues = [Clue(q: "Who is speaking? And to whom?",
-                                  a: "Jesus is speaking to his disciples."),
-                             Clue(q: "Where are they?", a: "The Last Supper."),
-                             Clue(q: "What metaphor is Jesus using?",
-                                  a: "He is the vine. We are the branches."),
-                             Clue(q: "Without Jesus, what can't we do?",
-                                  a: "We can't bear fruit."),
-                             Clue(q: "What's the first word?", a: "Apart")]
-                let verse = Verse(beats, prompts: cues, clues: clues,
-                                  dueDate: Date.distantPast)
-                modelContext.insert(verse)
-            }
-            Spacer()
-            
-            // (Goal) The coder can add a realistic verse with a past-due date.
-            Button("Add realistic verse. With past-due date") {
-                // (GNT) (Mark 1:40)
-                // (This disease was considered to make a person ritually unclean.)
-                // (pity; some manuscripts have anger.)
-                // short ver should be ??
-                let text = """
-                    A man suffering from a dreaded skin disease came to Jesus, knelt down, and begged him for help. 
-
-                    “If you want to,” he said, “you can make me clean.”
-                                  
-                    Jesus was filled with pity, and reached out and touched him. 
-
-                    “I do want to,” he answered.
-                    """
-                let beats = """
-                    A man suffering from 
-                    
-                    a dreaded skin disease 
-                    
-                    came to Jesus, 
-                    
-                    knelt down, and 
-                    
-                    begged him for help. 
-
-                    “If you want to,” he said, 
-                    
-                    “you can make me clean.”
-                                  
-                    Jesus was filled with pity, and 
-                    
-                    reached out and touched him. 
-
-                    “I do want to,” he answered.
-                    """
-                // what's the context? unclean, touch, Jesus' clean > my unclean, had to yell unclean, unsafe, fear,
-                let prompts = [Prompt(text: "When people are afraid of me.", clarifier: "A leper goes to Jesus.")]
-                let verse = Verse(beats, prompts: prompts,
-                                  dueDate: Date.distantPast)
-                modelContext.insert(verse)
-            }
-            Spacer()
+//            
+//            // (Goal) The coder can add a user verse with a distant due date.
+//            Button("Add user verse, with distant due date") {
+//                let verse = Verse("A message from the future", dueDate: Date.distantFuture)
+//                modelContext.insert(verse)
+//            }
+//            Spacer()
+//
+//            // (Goal) The coder can add a user verse with a past-due date.
+//            Button("Add user verse, with past-due date") {
+//                let verse = Verse("A message from the past", dueDate: Date.distantPast)
+//                modelContext.insert(verse)
+//            }
+//            Spacer()
+//            
+//            // (Goal) The coder can add a shorter, realistic verse with a past-due date.
+//            Button("Add realistic verse. Shorter. With past-due date") {
+//                // (NIV) (John 15)
+//                let text = """
+//                    Apart from me you can do nothing. 
+//                    """
+//                let beats = """
+//                    "Apart from me 
+//                    
+//                    you can do nothing."
+//                    """
+//                let cues = [Prompt(text: "When I forget about Jesus.", clarifier: "")]
+//                let clues = [Clue(q: "Who is speaking? And to whom?",
+//                                  a: "Jesus is speaking to his disciples."),
+//                             Clue(q: "Where are they?", a: "The Last Supper."),
+//                             Clue(q: "What metaphor is Jesus using?",
+//                                  a: "He is the vine. We are the branches."),
+//                             Clue(q: "Without Jesus, what can't we do?",
+//                                  a: "We can't bear fruit."),
+//                             Clue(q: "What's the first word?", a: "Apart")]
+//                let verse = Verse(beats, prompts: cues, clues: clues,
+//                                  dueDate: Date.distantPast)
+//                modelContext.insert(verse)
+//            }
+//            Spacer()
+//            
+//            // (Goal) The coder can add a realistic verse with a past-due date.
+//            Button("Add realistic verse. With past-due date") {
+//                // (GNT) (Mark 1:40)
+//                // (This disease was considered to make a person ritually unclean.)
+//                // (pity; some manuscripts have anger.)
+//                // short ver should be ??
+//                let text = """
+//                    A man suffering from a dreaded skin disease came to Jesus, knelt down, and begged him for help. 
+//
+//                    “If you want to,” he said, “you can make me clean.”
+//                                  
+//                    Jesus was filled with pity, and reached out and touched him. 
+//
+//                    “I do want to,” he answered.
+//                    """
+//                let beats = """
+//                    A man suffering from 
+//                    
+//                    a dreaded skin disease 
+//                    
+//                    came to Jesus, 
+//                    
+//                    knelt down, and 
+//                    
+//                    begged him for help. 
+//
+//                    “If you want to,” he said, 
+//                    
+//                    “you can make me clean.”
+//                                  
+//                    Jesus was filled with pity, and 
+//                    
+//                    reached out and touched him. 
+//
+//                    “I do want to,” he answered.
+//                    """
+//                // what's the context? unclean, touch, Jesus' clean > my unclean, had to yell unclean, unsafe, fear,
+//                let prompts = [Prompt(text: "When people are afraid of me.", clarifier: "A leper goes to Jesus.")]
+//                let verse = Verse(beats, prompts: prompts,
+//                                  dueDate: Date.distantPast)
+//                modelContext.insert(verse)
+//            }
+//            Spacer()
         }
-        .padding(.vertical)
+//        .padding(.vertical)
 
     }
+    
+    // MARK: - (buttons)
+
+    // (Goal) The coder can delete all persistent data without going thru Terminal and deleting all preview data. Latter takes 30" to rebuild.
+    @ViewBuilder
+    private var deleteDataButton: some View {
+        Button("Delete all persistent data") {
+            do {
+                // Works. But preview may crash.
+                try modelContext.container.erase()
+            } catch {
+                print(".erase() failed: \(error)")
+            }
+        }
+    }
+
+    // (Goal) The tester can add a user text of multiple paragraphs.
+    @ViewBuilder
+    private var addMultiverseButton: some View {
+        Button("Add multiverse") {
+            let text = Passage(
+                before: """
+                    \"… Neither can you bear fruit unless you remain in me.\"
+                    """,
+                goal: """
+                    \"I am the 
+                    vine; 
+                    you are the 
+                    branches. 
+                    
+                    If you remain in me 
+                    and I in you, 
+                    you will bear 
+                    much fruit; 
+                    
+                    apart from me 
+                    you can do 
+                    nothing.\"
+                    """,
+                after: "\"If you do not remain in me, you are like a branch that is thrown away and withers …\"",
+                reference: """
+                    (John) (NIV)
+                    """
+            )
+            modelContext.insert(text)
+        }
+    }
+    
+    // (Goal) The tester can add a user text of one paragraph.
+    @ViewBuilder
+    private var addUniverseButton: some View {
+        Button("Add universe") {
+            let text = Passage(
+                // TODO: - choose and format universe
+                // then also put into AppConstant.ExampleText
+                before: """
+                    ??
+                    """,
+                goal: """
+                    ??
+                    
+                    """,
+                after: "?? …",
+                reference: """
+                    (??John) (NIV)
+                    """
+            )
+            modelContext.insert(text)
+        }
+    }
+
+    @Environment(\.modelContext) private var modelContext
+}
+
+// MARK: - (preview)
+#Preview {
+    DebugView()
+        .modelContainer(for: Passage.self)
 }
