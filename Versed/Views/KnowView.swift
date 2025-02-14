61514f198dc53c047f73af12c3e4755f48b20a14
Geoff Hom <geoff.hom+git@gmail.com>
2025-02-14 00:33:45 -0600

    (WhenNextView) (if no quiz due, show when next)

--- Versed/Views/KnowView.swift
+++ Versed/Views/KnowView.swift
@@ -6,21 +6,13 @@ struct KnowView: View {
 // (goal) (user can get quizzed on her texts) (to know them)
     // MARK: - (body)
     var body: some View {
-//        Text("Know")
-        // how many paragraphs due?
-        // if zero, show view for when next one is due
-        
-        // or could say, when is next due? if not yet, show that view
-        
-        // suppose one is due; and it's the most due; how much do we want to show about others due?
-        // what's best for the user?
         
         // anki shows #due and #new to learn today
         // it also shows it while reviewing, which is definitely distracting
         // we also have the tab badge; if distracting, could do quiz as sheet
         
         // so try just jumping into most-due quiz
-        // so it's a bool; isParagraphDue (computed or not?)
+
         if isAnyParagraphDue {
             QuizView()
         } else {
@@ -28,7 +20,7 @@ struct KnowView: View {
         }
     }
     
-    // MARK: - (??)
+    // MARK: - (isAnyParagraphDue)
     
     private var isAnyParagraphDue: Bool {
         print("(KnowView) (isAnyParagraphDue) \(Date.now)")
@@ -36,14 +28,12 @@ struct KnowView: View {
         // (but at least it's not called continuously)
         // TODO: - (what about when a passage is updated? when it's paragraphs are updated?)
         
-        let mostDue = paragraphs.min { $0.dueDate < $1.dueDate }
-        return mostDue?.dueDate ?? Date.distantFuture < Date.now
+        let nextParagraph = paragraphs.min { $0.dueDate < $1.dueDate }
+        return nextParagraph?.dueDate ?? Date.distantFuture < Date.now
     }
     
     @Query
     private var paragraphs: [Paragraph]
-    
-//    @Environment(\.modelContext) private var modelContext
 }
 
 // MARK: - (preview)
--- Versed/Views/WhenNextView.swift
+++ Versed/Views/WhenNextView.swift
@@ -1,10 +1,26 @@
 import SwiftUI
+import SwiftData
 
 // MARK: - (WhenNextView)
 struct WhenNextView: View {
 // (goal) (user can see when the next quiz is)
     // MARK: - (body)
     var body: some View {
-        Text("WhenNextView")
+        Text("Nothing is due.")
+        if let nextParagraph {
+            Text("\nNext quiz:")
+            Text("\(nextParagraph.dueDate)")
+        }
     }
+   
+    // MARK: - (nextParagraph)
+    
+    private var nextParagraph: Paragraph? {
+    // (goal) (user sees when the next paragraph is due)
+    // (note) (after a user adds their first text, this should always be non-nil)
+        paragraphs.min { $0.dueDate < $1.dueDate }
+    }
+    
+    @Query
+    private var paragraphs: [Paragraph]
 }
